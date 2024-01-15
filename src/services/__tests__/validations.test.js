8a22137e0f93ef93c45108eb9f2cc0dabb808b9f
khelifa yassin <yassin.khelifa@student.vinci.be>
2024-01-15 16:43:18 +0100

    feat: gamertag check special charac >= 1

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -2,8 +2,13 @@
 const { isValid } = require('../validations');
 
 describe('validations tests suites - isValid', () => {
-    test('should return true', () => {
+    test('should return false when given 8 characters', () => {
         const result = isValid('12345678');
+        expect(result).toBe(false);
+    });
+
+    test('should return true when given 1 special character', () => {
+        const result = isValid('1234567&');
         expect(result).toBe(true);
     });
 });
